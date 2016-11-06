c1b5863e865b782da5af5b975f0cf7cd9b8239be
Amanda Richwalski <amandarichwalski@admins-MacBook-Pro.local>
2016-11-06 18:17:58 -0500

    first draft

---
+++ src/main/java/com/modaboutique/java/Application.java
@@ -0,0 +1,13 @@
+package com.modaboutique.java;
+
+
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		//SpringApplication.run(Application.class, args);
+
+	}
+
+}
---
+++ src/main/java/com/modaboutique/java/Security.java
@@ -0,0 +1,10 @@
+package com.modaboutique.java;
+
+public class Security {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
